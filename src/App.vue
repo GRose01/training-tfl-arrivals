789a20be89311fb2fa4fbbfcc0e71dc97fdbe491
Gabriella Rose <gabriella.sophie.rose@gmail.com>
2019-12-03 15:48:04 +0000

    successfully renders info from tfl api

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,37 @@
 <template>
   <div id="main-app" class="container">
     <h2>{{title}}</h2>
-    <!-- <h4>{{currentTime}}</h4> -->
+    <h4>{{currentTime}}</h4>
     <div v-for="(item, i) in tubeLine" v-bind:key="i">
-      <h4>{{tubeLine}}</h4>
-      <p>{{i}}</p>
+      <!-- <h4>{{tubeLine}}</h4> -->
+      <p>{{i}}</p> 
+      <p>{{item.id}}</p> 
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-// import moment from "moment";
+import moment from "moment";
+
+// const ids = "circle"
 
 export default {
   name: "MainApp",
   data () {
     return {
       title: "Arrivals board",
-      currentTime: "moment().format('MMMM Do YYYY, h:mm:ss a')",
-      tubeLine: []
+      currentTime: moment("dddd, MMMM Do YYYY, h:mm:ss a"),
+      tubeLine: [],
+      line: "circle",
+      mode: "tube",
+      serviceType: "Night"
     };
   },
-  // computed: {
-  //   currentTime() {
-  //     return moment().format('MMMM Do YYYY, h:mm:ss a');
-  //   }
-  // },
   mounted() {
     axios
-      .get('https://api.tfl.gov.uk/Line/bakerloo/Arrivals/940GZZLUWLO?direction=inbound')
+      // .get('https://api.tfl.gov.uk/Line/' + this.line)
+      .get('https://api.tfl.gov.uk/Line/Mode/' + this.mode + '/Route?serviceTypes=' + this.serviceType)
       .then(response => (this.tubeLine = response.data))
       // .catch(error => console.log(error))
   }
