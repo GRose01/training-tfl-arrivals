3acbe8d59a07f567ab425232f4b97357312a45f3
Gabriella Rose <gabriella.sophie.rose@gmail.com>
2019-12-04 12:10:10 +0000

    added time to arrival in minutes. Started to restructure components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="main-app" class="container">
     <h2>{{title}}</h2>
+    <h4></h4>
     <!-- <h4>{{currentTime}}</h4> -->
     <div v-for="(item, i) in tubeLine" v-bind:key="i">
         <table>
@@ -22,6 +23,7 @@
 import axios from "axios";
 // import moment from "moment";
 
+
 export default {
   name: "MainApp",
   data () {
@@ -30,14 +32,40 @@ export default {
       // currentTime: moment("dddd, MMMM Do YYYY, h:mm:ss a"),
       tubeLine: [],
       stopPoint: "940GZZLUASL",
-      serviceType: "Night"
+      serviceType: "Night",
+      refreshInterval: '',
+      // arrivalTime: this.getMinutes()
     };
   },
+
+  methods: {
+      getTubeInfo() {
+      axios
+        .get('https://api.tfl.gov.uk/StopPoint/' + this.stopPoint + '/Arrivals')
+        .then(response => (this.tubeLine = response.data))
+        // .catch(error => console.log(error))
+    },
+    // getMinutes() {
+    //   this.tubeline.forEach((value, index) => {
+    //     const mins = Math.floor(index.timeToStation/60);
+    //     const arrivingIn = mins === 0 ? "Due" : mins + "mins";
+
+    //     return arrivingIn
+    //   });
+    // },
+    updateTubeInfo() {
+      // clear board
+
+      // use get tube info?
+
+    }
+  },
   mounted() {
-    axios
-      .get('https://api.tfl.gov.uk/StopPoint/' + this.stopPoint + '/Arrivals')
-      .then(response => (this.tubeLine = response.data))
-      // .catch(error => console.log(error))
+    this.getTubeInfo()
+  // },
+  // updated() {
+  //   this.refreshInterval = setInterval(() => {
+  //   }, 10000);
   }
 };
 </script>
