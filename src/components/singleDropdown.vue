244359b2f0bfee28d98ba7fae39d8fc256286f74
Gabriella Rose <gabriella.sophie.rose@gmail.com>
2020-01-27 17:26:06 +0000

    Created a dropdown component in order to pass informations got from the api calls to the components using props and sending back relevant data for the next api call. This tidied up the app.vue file as you only need to include the component and data being passed to it.

---
+++ src/components/singleDropdown.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="dropdown">
+      <label for="forLabel" class="label">{{ labelText }}</label>
+      <select v-model="selectedItem" v-on:change="selectItem">
+        <option value="">{{ initialValue }}</option>
+        <option v-for="option in options" :key="option.value" :value="option.value">{{ option.name }}</option>
+      </select>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        forLabel: {
+            type: String,
+            required: true 
+        },
+        labelText: {
+            type: String,
+            required: true 
+        },
+        initialValue: {
+            type: String,
+            required: true 
+        },
+        options: {
+            type: Array,
+            required: true 
+        }
+    }, 
+    data () {
+        return {
+            selectedItem: ''
+        }
+    },
+    methods: {
+        selectItem() {
+            this.$emit('itemChanged', this.selectedItem)
+        }
+    }
+}
+
+</script>
