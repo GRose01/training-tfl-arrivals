e6c50fb52dce2a0aa74f5b03614603cb1b9782cd
Gabriella Rose <gabriella.sophie.rose@gmail.com>
2020-01-30 15:10:47 +0000

    added in a test specific to the project but Synax error is being thrown

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,6 +1,17 @@
+import Vue from 'vue';
+import App from '../src/App.vue';
 
 describe("App", () => {
     it("should fail", () => {
       expect(true).toBe(false);
     });
   });
+
+describe('App', () => {
+    // mount the component with a wrapper variable to render it
+    const wrapper = new Vue(MyComponent).$mount(App)
+
+    it('contains a dropdown', () => {
+        expect(wrapper.contains('dropdown')).toBe(true)
+    })
+})
